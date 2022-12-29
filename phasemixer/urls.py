36d3cd51c26c4f38dc1002e725feee02f823afe4
Ibrahim <imimazim7@gmail.com>
2022-12-29 02:33:32 +0200

    feat: created upload images endpoint

--- phasemixer/urls.py
+++ phasemixer/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path("home", views.home, name="home"),
     path("test", views.test, name='test'),
+    path("upload", views.upload, name='upload'),
 ]
--- phasemixer/views.py
+++ phasemixer/views.py
@@ -27,5 +27,21 @@ def test(request):
             }
         )
 
+@csrf_protect
+@csrf_exempt
+def upload(request):
+    if request.method == 'POST':
+        file = request.FILES['file']
+        location = request.POST['location']
+        file_path = "phasemixer/static/images/" + location + ".png"
+
+        img = Image.open(file)
+        img.save(file_path)
+        return JsonResponse(
+            {
+                'Status': "Saved at" + file_path
+            }
+        )
+
 
 
